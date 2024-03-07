5e513dbd6c38d94854fa0550b001f09bdb714811
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2024-03-07 12:46:07 -0300

    setup project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-green-800 flex justify-center items-center min-h-screen">
+
+    <h1 class="text-white text-2xl animate-pulse">hellow wrold</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>

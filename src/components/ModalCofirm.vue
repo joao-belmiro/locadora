a40e37f257d97f6a96b5ce7b0080f1c085110f05
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2024-03-09 21:23:13 -0300

    feat: add backgound opactity to overlay modal

--- src/components/ModalCofirm.vue
+++ src/components/ModalCofirm.vue
@@ -3,7 +3,7 @@
     v-if="hasClosed"
     id="popup-modal"
     tabindex="-1"
-    class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full flex"
+    class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full flex bg-gray-500 bg-opacity-50"
   >
     <div class="relative p-4 w-full max-w-md max-h-full">
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">

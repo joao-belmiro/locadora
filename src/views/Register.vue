a831f5696f06b660e2c375d96f09fa4f9b935ab7
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2024-03-10 12:39:01 -0300

    fix: repair register form

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -84,7 +84,7 @@
           </button>
           <router-link
             to="/"
-s            class="flex w-48 justify-center rounded-md bg-slate-600 px-3 p-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            class="flex w-48 justify-center rounded-md bg-slate-600 px-3 p-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Voltar 
           </router-link>

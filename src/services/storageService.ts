def0d3f3126c151c853bcc45e4fd2de48735ca95
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2024-03-11 11:53:33 -0300

    feat: create abstract crud class

---
+++ src/services/storageService.ts
@@ -0,0 +1,52 @@
+export default class LocalStorage<T> {
+    private storageKey: string;
+    
+    constructor(storageKey: string) {
+        this.storageKey = storageKey;
+    }
+
+    create(item: T): void {
+        const items = this.getAll();
+        items.push(item);
+        localStorage.setItem(this.storageKey, JSON.stringify(items));
+    }
+
+    read(id: number): T | null {
+        const items = this.getAll();
+        const index: number = items.findIndex(item => item.id === id)
+
+        if (id >= 0 && id < items.length) {
+            return items[index];
+        }
+        return null;
+    }
+
+    update(id: number, newItem: T): void {
+        const items = this.getAll();
+        const index: number = items.findIndex(item => item.id === id)
+        if (index >= 0 && id < items.length) {
+            items[index] = newItem;
+            localStorage.setItem(this.storageKey, JSON.stringify(items));
+        }
+    }
+
+    delete(id: number): void {
+        const items = this.getAll().filter(item => item.id !== id);
+        if (id !== 0) {
+            localStorage.setItem(this.storageKey, JSON.stringify(items));
+        }
+    }
+
+    getAll(): T[] {
+        const itemsJSON = localStorage.getItem(this.storageKey);
+        if (itemsJSON) {
+            return JSON.parse(itemsJSON) as T[];
+        }
+        return [];
+    }
+
+    getSize(): number {
+        const items = this.getAll();
+        return items.length;
+    }
+}

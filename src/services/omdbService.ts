70b21f682b3408df0d87602b768739512f4c5b58
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2024-03-10 22:46:27 -0300

    feat: create viacep and omdbService

---
+++ src/services/omdbService.ts
@@ -0,0 +1,11 @@
+import axios, { AxiosInstance } from "axios";
+
+
+const http: AxiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_OMDB_BASE_URL,
+    timeout: 30000
+})
+
+export async function getCep(cep:string) {
+    return http.get(`/${cep}/json`)
+}

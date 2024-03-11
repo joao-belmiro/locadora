8673d0b67dcabe1ed4626b8c8339da3ee0c122d8
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2024-03-11 15:40:58 -0300

    fix: adjust padding horizontal

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -7,8 +7,8 @@
         class="text-xs text-white uppercase bg-gray-50 dark:bg-gray-500 dark:text-white"
       >
         <tr>
-          <th v-for="col in columns" :key="col.label" scope="col" class="px-6 py-3">{{ col.label }}</th>
-          <th scope="col" class="px-6 py-3">Ações</th>
+          <th v-for="col in columns" :key="col.label" scope="col" class="px-2 py-3">{{ col.label }}</th>
+          <th scope="col" class="px-2 py-3">Ações</th>
         </tr>
       </thead>
       <tbody>
@@ -16,9 +16,9 @@
           class="border-b dark:bg-gray-400 text-slate-900 dark:border-gray-500 hover:bg-gray-50 dark:hover:bg-gray-400"
         >
           
-          <td v-for="col in columns" :key="col.label" class="px-6 py-4">{{ row[col.property] }}</td>
+          <td v-for="col in columns" :key="col.label" class="px-2 py-4">{{ row[col.property] }}</td>
           
-          <td class="flex items-center px-6 py-4 gap-4">
+          <td class="flex items-center px-2 py-4 gap-4">
             <button @click="doEdit(row)"
               class="inline-flex items-center px-2 py-1 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800"
             >

f78f3763dfa2a21e4798c4d548fc28f3d3ca0464
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2024-03-11 21:34:26 -0300

    feat: impl filters client

--- src/views/Client.vue
+++ src/views/Client.vue
@@ -5,12 +5,16 @@
         text-add="Novo Cliente"
         router-add="/new-client"
         status-negative="inativo"
-        status-positive="ativo">
+        status-positive="ativo"
+        @emit-search="searchbyName"
+        @emit-status="searchByStatus">
         <div class="md:w-40 sm:w-full">
             <label class="text-sm" for="document">Documento</label>
             <input
             id="document"
             type="number"
+            v-model="documento"
+            @input="searchByDoc"
             placeholder="Documento"
             class="block w-full  rounded-md border-0 py-1.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
@@ -56,7 +60,7 @@ const columns = ref<any[]>([
 const dataToDelete = ref<Client>();
 const data = ref<any[]>([])
 const storageClient = new LocalStorage<Client>('client')
-
+const documento = ref<number>(null)
 
 onMounted((): void => {
   getAllClients()
@@ -96,6 +100,22 @@ const formatClient = (data: any[]): any[] => {
 }
 const doEdit = (client: Client): void => {
   router.push(`/edit-client/${client.id}`)
+}
+const searchByDoc = () => {
+  filterBy('cpf', documento.value.toString())
+}
+const searchbyName = (name: string) => {
+  filterBy('nome', name)
+}
+
+const searchByStatus = (status: string) => {
+  const clients = storageClient.getAll().filter(client => client.status !== status)
+  data.value = formatClient(clients)
+}
+const filterBy = (key: string, value: string): void => {
+  const clients = storageClient.getAll().filter(client => client[key].indexOf(value) !== -1)
+  data.value = formatClient(clients)
+
 }
 </script>
   
